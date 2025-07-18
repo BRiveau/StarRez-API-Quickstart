0f213fbdefc9b7f757ec83525857941d0d1f9a03
briveau <briveau@yahoo.com>
2025-07-18 10:00:49 -0400

    Added scalar support

--- apps/backend/reverse-proxy/reverse-proxy/Program.cs
+++ apps/backend/reverse-proxy/reverse-proxy/Program.cs
@@ -1,16 +1,75 @@
+using Yarp.ReverseProxy.Transforms;
+using Yarp.ReverseProxy.Management;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add App Settings Files (Contains reverse proxy configuration)
+builder.Configuration.AddJsonFile("appsettings.json", true, true);
+builder.Configuration.AddJsonFile(
+    $"appsettings.{builder.Environment.EnvironmentName}.json",
+    true,
+    true
+    );
+
+// Initial reverse proxy setup
+var reverseProxyConfig = builder.Configuration.GetSection("ReverseProxy");
+builder.Services.AddReverseProxy()
+  .ConfigureHttpClient((context, handler) =>
+  {
+      handler.AutomaticDecompression = System.Net.DecompressionMethods.All;
+  })
+  .LoadFromConfig(reverseProxyConfig);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(
+        options =>
+        {
+            // Sets documentation website title, version, and description
+            options.AddDocumentTransformer((document, context, cancellationToken) =>
+                      {
+                          document.Info = new()
+                          {
+                              Title = "Reverse Proxy",
+                              Version = "v1",
+                              Description = "Reverse proxy for handling interaction with all APIs (default is configured with StarRez API support)"
+                          };
+                          return Task.CompletedTask;
+                      });
+
+            // Allows for Scalar documentation transformer features
+            options.AddScalarTransformers();
+        }
+        );
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Generates OpenAPI documentation for application
+app.MapOpenApi();
+
+// Adds API documentation for all specified clusters
+var clusterConfig = builder.Configuration.GetSection("ReverseProxy:Clusters").GetChildren();
+app.MapScalarApiReference("/docs", (options) =>
 {
-    app.MapOpenApi();
-}
+    options.ProxyUrl = $"{Environment.GetEnvironmentVariable("API_URL") ?? ""}/scalar-proxy";
+    foreach (var cluster in clusterConfig)
+    {
+        var clusterName = cluster.Key;
+        var clusterDestinations = cluster.GetChildren().ToArray()[0].GetChildren();
+        foreach (var destination in clusterDestinations)
+        {
+            if (clusterName.Contains("API"))
+            {
+                options.AddDocument("v1", clusterName, $"{clusterName.Split(' ')[0].ToLower()}/docs");
+                if (clusterName == "StarRez Internal API")
+                {
+                    options.AddDocument("v1", "StarRez Native API", $"{clusterName.Split(' ')[0].ToLower()}/native/docs");
+                }
+            }
+        }
+    }
+});
 
 app.UseHttpsRedirection();
 
@@ -21,7 +80,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
